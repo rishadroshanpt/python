c5181c07c5c89802e2741ff03781438f4e415d26
rishadroshanpt <rishadroshan27778@gmail.com>
2024-08-06 22:40:54 +0530

    var added

---
+++ demo.py
@@ -0,0 +1,28 @@
+roll_no=43
+print(type(roll_no))
+print("Roll no: ",roll_no)
+weight=78.5
+print(type(weight))
+print("Weight: ",weight)
+comp=12+2j
+print(type(comp))
+print("Complex: ",comp)
+name="Reacher"
+print(type(name))
+print("Name: ",name)
+a=True
+b=False
+print(type(a))
+print(a,b)
+names=["achu","appu","ammu"]
+print(type(names))
+print(names)
+names2=("achu","appu","ammu")
+print(type(names2))
+print(names2)
+names3={"achu","appu","ammu"}
+print(type(names3))
+print(names3)
+data={"name2":"anu"}
+print(type(data))
+print(data)
---
+++ demo2.py
@@ -0,0 +1,22 @@
+# Name=input("Enter your name : ")
+# Age=int(input("Enter age : "))
+# Weight=float(input("Enter your age : "))
+# print("Name : ",Name)
+# print("Age : ",Age)
+# print("Weight : ",Weight)/
+
+'''a=1234
+print("a",type(a),a)
+b=str(a)
+print("b",type(b),b)
+c=float(a)
+print("c",type(c),c)'''
+
+
+# print("""Welcome
+# to
+# all""")
+
+item=input("Enter item : ")
+price=float(input("Enter price"))
+print(f"Its an {item} , its price is ₹{price}")
