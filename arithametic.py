e02f12254db065864dd20b66ee1e628fe1977240
rishadroshanpt <rishadroshan27778@gmail.com>
2024-08-07 15:54:13 +0530

    arithmatic added

---
+++ arithametic.py
@@ -0,0 +1,83 @@
+# print(10+5)
+# print(10-5)
+# print(10/5)
+# print(10*5)
+# print(10%5)
+# print(10//5)
+# print(10**5)
+# print('Anu '+'mol')
+# print('Anu \n'*3)
+
+# a=10
+# print(a)
+# a=10
+# a+=5
+# print(a)
+# a=10
+# a-=5
+# print(a)
+# a=10
+# a*=5
+# print(a)
+# a=10
+# a/=5
+# print(a)
+# a=10
+# a%=5
+# print(a)
+# a=10
+# a//=5
+# print(a)
+# a=10
+# a**=2
+# print(a)
+
+
+# print("10==10  ",10==10)
+# print("10==9  ",10==9)
+# print("10!=10  ",10!=10)
+# print("10!=9  ",10!=9)
+# print("10<10  ",10<10)
+# print("10<11  ",10<11)
+# print("10>10  ",10>10)
+# print("10>9  ",10>9)
+# print("10<=10  ",10<=10)
+# print("10<=11  ",10<=11)
+# print("10<=9  ",10<=9)
+# print("10>=10  ",10>=10)
+# print("10>=11  ",10>=11)
+# print("10>=9  ",10>=9)
+
+# print("10>=10 and 10>=9 -> ",10>=10 and 10>=9)
+# print("10>=10 and 10>=11 -> ",10>=10 and 10>=11)
+# print("10>=11 and 10>=9 -> ",10>=11 and 10>=9)
+# print("10>=11 and 10>=12 -> ",10>=11 and 10>=12)
+# print("10>=10 or 10>=9 -> ",10>=10 or 10>=9)
+# print("10>=10 or 10>=11 -> ",10>=10 or 10>=11)
+# print("10>=11 or 10>=9 -> ",10>=11 or 10>=9)
+# print("10>=11 or 10>=12 -> ",10>=11 or 10>=12)
+# print("not 10==10  -> ",not(10==10))
+# print("not 10==9  -> ",not(10==9))
+
+
+# print('e in welcome  -> ','e'in'welcome')
+# print('p in welcome  -> ','p'in'welcome')
+# print('e not in welcome  -> ','e'not in'welcome')
+# print('p not in welcome  -> ','p'not in'welcome')
+
+
+a=10
+print('a ->',id(a))
+b=10
+print('b -> ',id(b))
+print('a==b  -> ',a==b)
+print('a is b  -> ',a is b)
+print('a is not b  -> ',a is not b)
+l=[10,20,30]
+print('l -> ',id(l))
+l1=[10,20,30]
+print('l1 -> ',id(l1))
+print('l==l1  -> ',l==l1)
+print('l is l1  -> ',l is l1)
+print('l is not l1  -> ',l is not l1)
+
