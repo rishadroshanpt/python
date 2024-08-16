cfd78b56997dad54fc15f33cd9c65f9c308b924f
rishadroshanpt <rishadroshan27778@gmail.com>
2024-08-16 16:12:21 +0530

    pattern added

---
+++ pattern/work1.py
@@ -0,0 +1,111 @@
+# for i in range(3):
+#     for j in range(3):
+#         print('*',end="  ")
+#     print()
+
+'''
+*  *  *
+*  *  *
+*  *  *
+'''
+
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(j+1,end="  ")
+#     print()
+
+'''
+1  2  3  
+1  2  3
+1  2  3
+'''
+
+
+
+# for i in range(5):
+#     if i%2==0:
+#         for j in range(3):
+#             print(i,end="  ")
+#         print()
+
+'''
+0  0  0  
+2  2  2
+4  4  4
+'''
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i+j+i+i,end="  ")
+#     print()
+
+# a=0
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="  ")
+#         a+=1
+#     print()
+
+'''
+0  1  2  
+3  4  5
+6  7  8
+'''
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i+j,end="  ")
+#     print()
+
+'''
+0  1  2  
+1  2  3
+2  3  4
+'''
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i+j+i,end="  ")
+#     print()
+
+
+# for i in range(5):
+#     if i%2==0:
+#         for j in range(3):
+#             print(i,end="  ")
+#             i+=1
+#         print()
+'''
+0  1  2  
+2  3  4
+4  5  6
+'''
+
+# for i in range(4):
+#     if i%2==0:
+#         for j in range(3):
+#             print(j,end="  ")
+#         print()
+#     else:
+#         for j in range(3):
+#             print(2-j,end="  ")
+#         print()
+        
+'''
+0  1  2  
+2  1  0
+0  1  2
+2  1  0
+'''
+for i in range(3):
+        for j in range(3):
+            print((i+j)**2,end="  ")
+        print()
+
+'''
+0  1  4  
+1  4  9
+4  9  16
+'''
