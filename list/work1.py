e5ac030a9e9759370e2afc556441fb646ace9db2
rishadroshanpt <rishadroshan27778@gmail.com>
2024-08-19 15:59:11 +0530

    list added

---
+++ list/work1.py
@@ -0,0 +1,62 @@
+# L=[1,2,'abc',2.3,2]
+# print('Length of the string is ',len(L))
+# value=input('Enter value to be found : ')
+# if value in L:
+#     print('Found.')
+# else:
+#     print('Not found')
+
+
+# l=[1,3,2,'we',2.4,4]
+# for i in l:
+#     print(i)
+
+
+# L=[1,2,'abc',2.3,2]
+# print('Length of the string is ',len(L))
+# print('2nd position content is ',L[2])
+# print('3rd position content is ',L[3])
+# L[3]=4
+# L[2]=3
+# print('After changing 2nd position content is ',L[2])
+# print('After changing 3rd position content is ',L[3])
+# print(L)
+
+
+# L=[]
+# L.append(10)
+# print(L)
+# L.append(30)
+# print(L)
+# L.append('a')
+# print(L)
+# L.append([10,20,30,40])
+# print(L)
+# L.extend([1,2,3,4])
+# print(L)
+# L.insert(2,50)
+# print(L)
+# L.insert(2,'a')
+# print(L)
+
+
+# l=[11,22,33,44,55,66]
+# print(l)
+# l.pop()
+# print(l)
+# l.pop(2)
+# print(l)
+# l.remove(44)
+# print(l)
+# l.clear()
+# print(l)
+
+
+l=[1,4,2,6,3,7,5,6,12]
+print(l)
+l.sort()
+print(l)
+l.reverse()
+print(l)
+print(l.count(6))
+print(l.index(5))
