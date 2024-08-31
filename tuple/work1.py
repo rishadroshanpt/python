648c3caa25170a1a2576ee0b4f0adfe2c841c853
rishadroshanpt <rishadroshan27778@gmail.com>
2024-08-31 09:02:05 +0530

    tuple added

--- tuple/work1.py
+++ tuple/work1.py
@@ -52,5 +52,52 @@
 #     print('No')
 
 
-a=10,
-print(a)
+# t=(10,[1,2],11)
+# print(t)
+# t[1].append(3)
+# print(t)
+
+'''         (10, [1, 2], 11)
+            (10, [1, 2, 3], 11)                  '''
+
+
+
+
+
+
+
+# t=(10,11,12,13)
+# print(t)
+# l=list(t)
+# l.pop()
+# l[-1]=111
+# t=tuple(l)
+# print(t)
+
+'''         (10, 11, 12, 13)
+            (10, 11, 111)               '''
+
+
+
+
+
+
+
+
+
+t=(1,2,3,4,3,5,2,1,3,2)
+a=int(input('Enter a value : '))
+c=t.count(a)
+print('count : ',c)
+pos=0
+while c>0:
+    p=t.index(a,pos)
+    pos=p+1
+    print(p)
+    c-=1
+
+'''         Enter a value : 2
+            count :  3
+            1
+            6
+            9                       '''
