a306130984266066a152e45ac532ec5c2a636068
rishadroshanpt <rishadroshan27778@gmail.com>
2024-09-18 08:21:30 +0530

    done

--- function/employee.py
+++ function/employee.py
@@ -20,6 +20,32 @@ def add_emp():
         salary=int(input('Enter salary : '))
         position=input('Enter position : ')
         emp.append({'id':id,'name':name,'dob':dob,'salary':salary,'position':position})
+def display_emp():
+    print('{:<5}{:<10}{:<10}{:<10}{:<10}'.format('ID','NAME','DOB','SALARY','POSITION'))
+    print('-'*45)
+    for i in emp:
+        print('{:<5}{:<10}{:<10}{:<10}{:<10}'.format(i['id'],i['name'],i['dob'],i['salary'],i['position']))
+def update_emp():
+    id=int(input('Enter id : '))
+    f=0
+    for i in emp:
+        if i['id']==id:
+            f=1
+            new_salary=int(input('Enter updated salary : '))
+            new_position=input('Enter new position : ')
+            i['salary']=new_salary
+            i['position']=new_position
+    if f==0:    
+        print('Id does not exists !')
+def delete_emp():
+    id=int(input('Enter id : '))
+    f=0
+    for i in emp:
+        if i['id']==id:
+            f=1
+            emp.remove(i)
+    if f==0:    
+        print('Id does not exists !')
 while True:
     print('''
 1.Login
@@ -38,8 +64,16 @@ while True:
                 sub_ch=int(input('Enter your choice : '))
                 if sub_ch==1:
                     add_emp()
-                elif ch==5:
+                elif sub_ch==2:
+                    display_emp()
+                elif sub_ch==3:
+                    update_emp()
+                elif sub_ch==4:
+                    delete_emp()
+                elif sub_ch==5:
                     break
+                else:
+                    print('Invalid choice !')
         elif f==0:
             print('incorrect password or username !')
     elif ch==2:
